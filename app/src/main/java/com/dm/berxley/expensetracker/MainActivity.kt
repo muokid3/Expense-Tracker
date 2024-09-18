49d813d0567dffd162309e8a0d137c72246a5c4b
Dennis Muoki <muokid3@gmail.com>
2024-09-18 15:30:04 +0300

    splash screen

--- app/src/main/java/com/dm/berxley/expensetracker/MainActivity.kt
+++ app/src/main/java/com/dm/berxley/expensetracker/MainActivity.kt
@@ -3,6 +3,7 @@ package com.dm.berxley.expensetracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -10,12 +11,28 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.dm.berxley.expensetracker.ui.theme.ExpenseTrackerTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val mainViewModel by viewModels<MainViewModel>()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+        installSplashScreen().apply {
+            setKeepOnScreenCondition {
+                mainViewModel.showSplash
+            }
+        }
+
         setContent {
+
             ExpenseTrackerTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
